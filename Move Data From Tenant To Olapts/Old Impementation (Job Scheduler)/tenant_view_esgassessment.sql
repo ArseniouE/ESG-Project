927d6fb2a1325393cc0c5369723243ce51c680d7
ibitsaki <bitsakii@unisystems.gr>
2023-11-14 13:54:18 +0200

    updates

--- Move Data From Tenant To Olapts/Old Impementation (Job Scheduler)/tenant_view_esgassessment.sql
+++ Move Data From Tenant To Olapts/Old Impementation (Job Scheduler)/tenant_view_esgassessment.sql
@@ -2,100 +2,94 @@ DROP VIEW IF EXISTS tenant.v_esgassessment;
 analyze tenant.esgassessment;
 create or replace view tenant.v_esgassessment as
 select
-         h.pkid_ as v_esgassessmentid_ -- create pkid_ and rename it as viewname||id_
-        ,h.pkid_::varchar as pkid_ -- pull pkid_ for transformation if this is latestversion view
-        ,h.id_::varchar as esgassessmentid_ -- pull the id_ from tenant table and name it as tenant.tablename||id_ 
-        -- add several jsonb columns here and alphabetize them in lowercase order
-		,(h.jsondoc_ ->>'ApprovalDate') AS ApprovalDate
-		,(h.jsondoc_ ->>'ApprovedUser') AS ApprovedUser
-		,(h.jsondoc_ ->>'AssessmentDate') AS AssessmentDate
-		,(h.jsondoc_ ->>'AssessmentUser') AS AssessmentUser
-		select * from tenant.esgassessment limit 10
-		,(h.jsondoc_->>'AssetsRecentValue') AS AssetsRecentValue
-		,(h.jsondoc_ ->>'AuthorizationFlag')::boolean AS AuthorizationFlag
-		,(h.jsondoc_ ->> 'GroupNaceCode') AS CompanyNaceCode
-		,(h.jsondoc_ ->> 'GroupName') AS CompanyName
-		,(h.jsondoc_ ->>'CreditCommitteeDate') AS CreditCommitteeDate
+         h.pkid_ as v_esgassessmentid_
+        ,h.pkid_::varchar as pkid_ 
+        ,h.id_::varchar as esgassessmentid_ 
+		,(h.jsondoc_ ->>'ApprovalDate') AS approvaldate
+		,(h.jsondoc_ ->>'ApprovalUser') AS approvaluser
+		,(h.jsondoc_ ->>'AssessmentDate') AS assessmentdate
+		,(h.jsondoc_ ->>'AssessmentUser') AS assessmentuser
+		,(h.jsondoc_->>'AssetsRecentValue') AS assetsrecentvalue
+		,(h.jsondoc_ ->>'AuthorizationFlag')::boolean AS authorizationflag
+		,(h.jsondoc_ ->> 'CompanyNaceCode') AS companynacecode
+		,(h.jsondoc_ ->> 'CompanyName') AS companyname
+		,(h.jsondoc_ ->>'CreditCommitteeDate') AS creditcommitteedate
 		--,(h.jsondoc_ ->>'DoesAnnualTurnoverExceed50')::boolean AS DoesAnnualTurnoverExceed50
 		--,(h.jsondoc_ ->>'DoesCreditLimitExceed55')::boolean AS DoesCreditLimitExceed55
 		--,(h.jsondoc_ ->>'DoesExposureExceed50')::boolean AS DoesExposureExceed50
-		,(h.jsondoc_ ->>'EbiodiversityScore') AS EbiodiversityScore
-		,(h.jsondoc_ ->>'EclimateGoalsTargetsScore') AS EclimateGoalsTargetsScore
-		,(h.jsondoc_ ->>'EemissionsScore') AS EemissionsScore
-		,(h.jsondoc_ ->>'EenergyScore') AS EenergyScore
-		,(h.jsondoc_ ->>'EenvironmentalComplianceScore') AS EenvironmentalComplianceScore
-		,(h.jsondoc_ ->>'EevaluationApproachScore') AS EevaluationApproachScore
-		,(h.jsondoc_ ->>'EmanagementApproachScore') AS EmanagementApproachScore
-		,(h.jsondoc_ ->>'EmanagementSystemsScore') AS EmanagementSystemsScore
-		,(h.jsondoc_ ->>'EmonitoringMechanismsScore') AS EmonitoringMechanismsScore
-		,(h.jasondoc_->>'EmployeesRecentValue') AS EmployeesRecentValue
-		,(h.jsondoc_ ->> 'EntityId') AS EntityId
-		,(h.jsondoc_ ->> 'EntityVersionId') AS EntityVersionId
-		,(h.jsondoc_ ->> 'EperformanceAssessmentScore') AS EperformanceAssessmentScore
-		,(h.jsondoc_ ->> 'EphysicalRiskAssessmentScore') AS EphysicalRiskAssessmentScore
-		,(h.jsondoc_ ->> 'EpoliciesScore') AS EpoliciesScore
-		,(h.jsondoc_ ->> 'EsgGrade') AS EsgGrade
-		,(h.jsondoc_ ->> 'EsgZonesOutcome') AS EsgZonesOutcome
-		,(h.jsondoc_ ->> 'EsubGrade') AS EsubGrade
-		,(h.jsondoc_ ->> 'EsubScore') AS EsubScore
-	--edw leipei to esubzone pou iparxei sto CL kai sto json tis tenant.esgassessment
-		,(h.jsondoc_ ->> 'EwasteScore') AS EwasteScore
-		,(h.jsondoc_ ->> 'EwaterEffluentsScore') AS EwaterEffluentsScore
-		,(h.jsondoc_ ->>'ExpirationDate') as ExpirationDate
-		,(h.jsondoc_ ->> 'EzoneOutcome') AS EzoneOutcome
-		,(h.jsondoc_ ->> 'GbusinessEthicsScore') AS GbusinessEthicsScore
-		,(h.jsondoc_ ->> 'GcollectiveKnowledgeScore') AS GcollectiveKnowledgeScore
-		,(h.jsondoc_ ->> 'GcompositionDiversityScore') AS GcompositionDiversityScore
-		,(h.jsondoc_ ->> 'GperformanceEvaluationScore') AS GperformanceEvaluationScore
-		,(h.jsondoc_ ->> 'GregulatoryComplianceScore') AS GregulatoryComplianceScore
-		,(h.jsondoc_ ->> 'GremunerationScore') AS GremunerationScore
-		,(h.jsondoc_ ->> 'GrolesResponsibilitiesScore') AS GrolesResponsibilitiesScore
+		,(h.jsondoc_ ->>'EbiodiversityScore') AS ebiodiversityscore
+		,(h.jsondoc_ ->>'EclimateGoalsTargetsScore') AS eclimategoalstargetsscore
+		,(h.jsondoc_ ->>'EemissionsScore') AS eemissionsscore
+		,(h.jsondoc_ ->>'EenergyScore') AS eenergyscore
+		,(h.jsondoc_ ->>'EenvironmentalComplianceScore') AS eenvironmentalcompliancescore
+		,(h.jsondoc_ ->>'EevaluationApproachScore') AS eevaluationapproachscore
+		,(h.jsondoc_ ->>'EmanagementApproachScore') AS emanagementapproachscore
+		,(h.jsondoc_ ->>'EmanagementSystemsScore') AS emanagementsystemsscore
+		,(h.jsondoc_ ->>'EmonitoringMechanismsScore') AS emonitoringmechanismsscore
+		,(h.jsondoc_->>'EmployeesRecentValue') AS employeesrecentvalue
+		,(h.jsondoc_ ->> 'EntityId') AS entityid
+		,(h.jsondoc_ ->> 'EntityVersionId') AS entityversionid
+		,(h.jsondoc_ ->> 'EperformanceAssessmentScore') AS eperformanceassessmentscore
+		,(h.jsondoc_ ->> 'EphysicalRiskAssessmentScore') AS ephysicalriskassessmentscore
+		,(h.jsondoc_ ->> 'EpoliciesScore') AS epoliciesscore
+		,(h.jsondoc_ ->> 'EsgGrade') AS esggrade
+		,(h.jsondoc_ ->> 'EsgZonesOutcome') AS esgzonesoutcome
+		,(h.jsondoc_ ->> 'EsubGrade') AS esubgrade
+		,(h.jsondoc_ ->> 'EsubScore') AS esubscore
+		,(h.jsondoc_->>'EsubZone') AS esubzone
+		,(h.jsondoc_ ->> 'EwasteScore') AS ewastescore
+		,(h.jsondoc_ ->> 'EwaterEffluentsScore') AS ewatereffluentsscore
+		,(h.jsondoc_ ->>'ExpirationDate') as expirationdate
+		,(h.jsondoc_ ->> 'EzoneOutcome') AS ezoneoutcome
+		,(h.jsondoc_ ->> 'GbusinessEthicsScore') AS gbusinessethicsscore
+		,(h.jsondoc_ ->> 'GcollectiveKnowledgeScore') AS gcollectiveknowledgescore
+		,(h.jsondoc_ ->> 'GcompositionDiversityScore') AS gcompositiondiversityscore
+		,(h.jsondoc_ ->> 'GperformanceEvaluationScore') AS gperformanceevaluationscore
+		,(h.jsondoc_ ->> 'GregulatoryComplianceScore') AS gregulatorycompliancescore
+		,(h.jsondoc_ ->> 'GremunerationScore') AS gremunerationscore
+		,(h.jsondoc_ ->> 'GrolesResponsibilitiesScore') AS grolesresponsibilitiesscore
 		--,(h.jsondoc_ ->> 'GroupId') AS GroupId
-		,(h.jsondoc_ ->> 'GstakeholderEngagementScore') AS GstakeholderEngagementScore		
-		,(h.jsondoc_ ->> 'GstrategyRiskManagementScore') AS GstrategyRiskManagementScore
-		,(h.jsondoc_ ->> 'GsubGrade') AS GsubGrade
-		,(h.jsondoc_ ->> 'GsubScore') AS GsubScore
-	--edw leipei to gsubzone pou iparxei kai sto CL kai sto json tis tenant.esgassessment
-		,(h.jsondoc_ ->> 'GtransparencyScore') AS GtransparencyScore
-		,(h.jsondoc_ ->> 'GzoneOutcome') AS GzoneOutcome
+		,(h.jsondoc_ ->> 'GstakeholderEngagementScore') AS gstakeholderengagementscore		
+		,(h.jsondoc_ ->> 'GstrategyRiskManagementScore') AS gstrategyriskmanagementscore
+		,(h.jsondoc_ ->> 'GsubGrade') AS gsubgrade
+		,(h.jsondoc_ ->> 'GsubScore') AS gsubscore
+		,(h.jsondoc_->>'GsubZone') AS gsubzone
+		,(h.jsondoc_ ->> 'GtransparencyScore') AS gtransparencyscore
+		,(h.jsondoc_ ->> 'GzoneOutcome') AS gzoneoutcome
 		--,(h.jsondoc_ ->> 'HaveFullBlownCriteriaBeenMet')::boolean AS HaveFullBlownCriteriaBeenMet
-		,(h.jsondoc_ ->> 'IsCompanyListedInStockExchange')::boolean  AS IsCompanyListedInStockExchange	
-		,(h.jsondoc_ ->> 'NextReviewDate') AS NextReviewDate
-		,(h.jsondoc_ ->> 'OverrideAuthority') AS OverrideAuthority
-		,(l1.jsondoc_ ->> 'Value') AS OverrideAuthorityVal 
-		,(h.jsondoc_ ->> 'OverrideDriver') AS OverrideDriver --ref_Dato esgOverrideDriver auto den einai ref_dato?? fainetai keno	
-		,(h.jsondoc_ ->> 'OverrideGrade') AS OverrideGrade --kai auto to deixnei san ref dato to CL
-		,(h.jsondoc_ ->> 'OverrideProvider') AS OverrideProvider
-		,(h.jsondoc_ ->> 'OverrideReason') AS OverrideReason --ref_Dato esgOverrideReason auto den einai ref_dato?? fainetai keno	
-		
-		--,(h.jsondoc->>'QuestionnaireDate') as QuestionnaireDate --iparxei kai sto json kai sto Cl
-		
-		--,(h.jsondoc_ ->> 'Id') AS QuestionnaireId ------auto einai to QuestionnaireId
-		
-		,(h.jsondoc_ ->> 'TypeQuestionnaire') AS QuestionnaireType	
-		--,(h.jsondoc_->>'QuestionnaireVersion') as QuestionnaireVersion --iparxei kai sto jsondoc kai sto cl
-		,(h.jsondoc_->>'RevenuesRecentValue') AS RevenuesRecentValue
-		,(h.jsondoc_ ->> 'ScomplianceScore') AS ScomplianceScore
-		,(h.jsondoc_ ->> 'ScustomerPrivacyScore') AS ScustomerPrivacyScore
-		,(h.jsondoc_ ->> 'SevaluationApproachScore') AS SevaluationApproachScore
-		,(h.jsondoc_ ->> 'SgoalsTargetsScore') AS SgoalsTargetsScore
-		,(h.jsondoc_ ->> 'ShumanRightsScore') AS ShumanRightsScore
-		,(h.jsondoc_ ->> 'SlaborManagementRelationsScore') AS SlaborManagementRelationsScore
-		,(h.jsondoc_ ->> 'SlocalCommunitiesScore') AS SlocalCommunitiesScore
-		,(h.jsondoc_ ->> 'SmanagementApproachScore') AS SmanagementApproachScore
-		,(h.jsondoc_ ->> 'SmanagementSystemsScore') AS SmanagementSystemsScore
-		,(h.jsondoc_ ->> 'SmonitoringMechanismsScore') AS SmonitoringMechanismsScore
-		,(h.jsondoc_ ->> 'SoccupationalHealthSafetScore') AS SoccupationalHealthSafetScore
-		,(h.jsondoc_ ->> 'SperformanceAssessmentScore') AS SperformanceAssessmentScore
-		,(h.jsondoc_ ->> 'SpoliciesScore') AS SpoliciesScore
-		,(h.jsondoc_ ->> 'SsubGrade') AS SsubGrade
-		,(h.jsondoc_ ->> 'SsubScore') AS SsubScore
-	--leipei to Ssubzone pou iparxei kai CL kai json 
-		,(h.jsondoc_ ->> 'StrainingEducationScore') AS StrainingEducationScore
-		,(h.jsondoc_ ->> 'SzoneOutcome') AS SzoneOutcome
-		,(h.jsondoc_ ->> 'TotalEsgScore') AS TotalEsgScore
-		
-		
+		,(h.jsondoc_ ->> 'IsCompanyListedInStockExchange')::boolean  AS iscompanylistedinstockexchange	
+		,(h.jsondoc_ ->> 'NextReviewDate') AS nextreviewdate
+		,(h.jsondoc_ ->> 'OverrideAuthority') AS overrideauthority --ref dato
+		,(l1.jsondoc_ ->> 'Value') AS overrideauthorityval 
+		,(h.jsondoc_ ->> 'OverrideDriver') AS overridedriver --ref_Dato esgOverrideDriver auto den einai ref_dato?? fainetai keno	
+		,(h.jsondoc_ ->> 'OverrideGrade') AS overridegrade 
+		,(l2.jsondoc_ ->> 'Value') AS overridegradeval 
+		,(h.jsondoc_ ->> 'OverrideProvider') AS overrideprovider
+		,(h.jsondoc_ ->> 'OverrideReason') AS overridereason --ref_Dato esgOverrideReason auto den einai ref_dato?? fainetai keno	
+		,(h.jsondoc_->>'QuestionnaireDate') as questionnairedate 
+		,(h.jsondoc_ ->> 'QuestionnaireId') AS questionnaireid 
+		,(h.jsondoc_ ->> 'QuestionnaireType') AS questionnairetype	
+		,(h.jsondoc_->>'QuestionnaireVersion') as questionnaireversion 
+		,(h.jsondoc_->>'RevenuesRecentValue') AS revenuesrecentvalue
+		,(h.jsondoc_ ->> 'ScomplianceScore') AS scompliancescore
+		,(h.jsondoc_ ->> 'ScustomerPrivacyScore') AS scustomerprivacyscore
+		,(h.jsondoc_ ->> 'SevaluationApproachScore') AS sevaluationapproachscore
+		,(h.jsondoc_ ->> 'SgoalsTargetsScore') AS sgoalstargetsscore
+		,(h.jsondoc_ ->> 'ShumanRightsScore') AS shumanrightsscore
+		,(h.jsondoc_ ->> 'SlaborManagementRelationsScore') AS slabormanagementrelationsscore
+		,(h.jsondoc_ ->> 'SlocalCommunitiesScore') AS slocalcommunitiesscore
+		,(h.jsondoc_ ->> 'SmanagementApproachScore') AS smanagementapproachscore
+		,(h.jsondoc_ ->> 'SmanagementSystemsScore') AS smanagementsystemsscore
+		,(h.jsondoc_ ->> 'SmonitoringMechanismsScore') AS smonitoringmechanismsscore
+		,(h.jsondoc_ ->> 'SoccupationalHealthSafetScore') AS soccupationalhealthsafetscore
+		,(h.jsondoc_ ->> 'SperformanceAssessmentScore') AS sperformanceassessmentscore
+		,(h.jsondoc_ ->> 'SpoliciesScore') AS spoliciesscore
+		,(h.jsondoc_ ->> 'SsubGrade') AS ssubgrade
+		,(h.jsondoc_ ->> 'SsubScore') AS ssubscore
+		,(h.jsondoc_->>'SsubZone') AS ssubzone
+		,(h.jsondoc_ ->> 'StrainingEducationScore') AS strainingeducationscore
+		,(h.jsondoc_ ->> 'SzoneOutcome') AS szoneoutcome
+		,(h.jsondoc_ ->> 'TotalEsgScore') AS totalesgscore
 		,h.wfid_::varchar
 		,h.taskid_::varchar
 		,h.versionid_::int4
@@ -113,17 +107,19 @@ select
 		,h.updateddate_::timestamp
 		,h.fkid_entity
 		 -- add these 2 columns to be used for incremental transformation and incremental dataextracts --
-		,(CASE WHEN h.updateddate_ > h.createddate_ THEN h.updatedby_ ELSE h.createdby_ END)::varchar AS sourcepopulatedby_,
+		,(CASE WHEN h.updateddate_ > h.createddate_ THEN h.updatedby_ ELSE h.createdby_ END)::varchar AS sourcepopulatedby_
 		,GREATEST(h.createddate_, h.updateddate_)::Timestamp AS sourcepopulateddate_
 	 FROM tenant.esgassessment h
 		left join tenant.custom_lookup l1 on l1.t_ = 'OverrideAuthority' and l1.jsondoc_->>'Key'  = h.jsondoc_ ->> 'OverrideAuthority'
-	where h.isvalid_ and h.isvisible_
+		left join tenant.custom_lookup l2 on l2.t_ = 'EsgOverrideGrade' and l2.jsondoc_->>'Key'  = h.jsondoc_ ->> 'OverrideGrade'
+	where h.isvalid_ and h.isvisible_;
 	--(h.isvalid_ and h.isvisible_ and h.islatestversion_ ); 
-	-- note: and not h.isdeleted_ this was removed in 20.31.1
-	-------
 
 
---select * from tenant.v_esgquestion
+
+
+
+--select * from tenant.v_esgassessment
 
 ALTER TABLE tenant.v_esgassessment OWNER TO tenant;
 GRANT ALL ON TABLE tenant.v_esgassessment TO tenant;
